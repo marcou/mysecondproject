092df60b3354c1b1488859e1d8c60dbbab98f57b
Szpada <michal.szmorlinski@gmail.com>
2012-08-19 15:03:29 +0200

    gowno

--- src/com/gra/minigra/Ball.java
+++ src/com/gra/minigra/Ball.java
@@ -26,40 +26,44 @@ public class Ball{
 
 	private MiniGame1View view;
 	
+	private float velocity;		//predkosc kulki
+	private float X_speed;		//predkosc w plaszczyznie X
+	private float Y_speed;		//predkosc w plaszczyznie Y
 	private float X_position;	//pozycja X
 	private float Y_position;	//pozycja Y
+	private float direction = 0;	//kierunek (stopnie 0 - 360)
 	
 	private float radius;		//promien
 	private float elasticity;	//elastycznosc 0.0 - 1.0
 	private float mass;			//masa kulki
 	private float gravity;		//grawitacja oddzialujaca na kulke
 	
-	private float X_speed;		//predkosc w plaszczyznie X
-	private float Y_speed;		//predkosc w plaszczyznie Y
+	
 	
 	private int life;			//zycie kulki
 	
 	private List<Ball> balls;
 	private Paint paint = new Paint();
 	
-	public Ball(MiniGame1View gameView, List<Ball> balls, float x_position, float y_position, float x_speed, float y_speed, float radius, float mass, float elasticity, int life){
+	public Ball(MiniGame1View gameView, List<Ball> balls, float x_position, float y_position, float velocity, float direction, float radius, float mass, float elasticity, int life){
 		
 		this.view = gameView;
 		
+		this.velocity = velocity;
+		this.direction = direction;
 		this.X_position = x_position;
 		this.Y_position = y_position;
+		
+		this.X_speed = (float)Math.cos(Math.toRadians(direction)) * this.velocity;
+		this.Y_speed = (float)Math.sin(Math.toRadians(direction)) * this.velocity;
 		
 		this.radius = radius;
 		this.elasticity = elasticity;
 		this.mass = mass;
 		
-		this.X_speed = x_speed;
-		this.Y_speed = y_speed;
-		
 		this.life = life;
 		
 		this.balls = balls;
-		
     	this.paint.setColor(Color.RED);
 	}
 	public void onDraw(Canvas canvas) {
@@ -71,37 +75,52 @@ public class Ball{
     	if(this.life < 1){
     		balls.remove(this);
     	}
+    	//X_speed = (float)Math.cos(direction) * velocity;
+    	//Y_speed = (float)Math.sin(direction) * velocity;
+    	X_position += X_speed;
+    	Y_position += Y_speed;
     	
-		X_position += X_speed;
-		Y_position += Y_speed;
+    	/*
+    	 * kolizja ze scianami
+    	 */
     	
-	    if(Y_position + radius < 800.0){
-	    	Y_speed += gravity;
-	    }
-	    if(Y_speed == 0){
-	    	X_speed *= 0.9;
-	    }
-	    
-	    if(X_position + radius > 480.0) {
-	    	X_position = 480.0f - radius;
-	        X_speed *= -1.0 * elasticity;
-	    }
-	    else if(X_position - radius < 0.0) {
-	        X_position = radius;
-	        X_speed *= -1.0 * elasticity;
-	    }
-	 
-	    if(Y_position + radius > 800.0) {
-	    	Y_position = 800.0f - radius;
-	        Y_speed *= -1.0 * elasticity;
-	        if(-Y_speed < gravity){
-	        	Y_speed = 0;
-	        }
-	    }
-	    else if(Y_position - radius < 0.0) {
-	    	Y_position = radius;
-	        Y_speed *= -1.0 * elasticity;
-	    }
+    	//lewa
+    	if(X_position - radius < 0){
+    		X_position = radius;							//pozycja X
+    		direction = 180 - direction;					//kierunek wedlug zasady kat odbicia = kat padania
+    		velocity *= elasticity;							//predkosc pomniejszona o elastycznosc
+    		
+    		X_speed = (float)Math.cos(Math.toRadians(direction)) * velocity;//wyliczenie predkosci w plaszczyznie X na nowo
+        	Y_speed = (float)Math.sin(Math.toRadians(direction)) * velocity;//wyliczenie predkosci w plaszczyznie Y na nowo
+    	}
+    	//prawa
+    	else if(X_position + radius > 480){
+    		X_position = 480 - radius;
+    		direction = 180 - direction;
+    		velocity *= elasticity;
+    		
+    		X_speed = (float)Math.cos(Math.toRadians(direction)) * velocity;
+        	Y_speed = (float)Math.sin(Math.toRadians(direction)) * velocity;
+    	}
+    	//gorna
+    	if(Y_position - radius < 0){
+    		Y_position = radius;
+    		direction = 360 - direction;
+    		velocity *= elasticity;
+    		
+    		X_speed = (float)Math.cos(Math.toRadians(direction)) * velocity;
+        	Y_speed = (float)Math.sin(Math.toRadians(direction)) * velocity;
+    	}
+    	//dolna
+    	else if(Y_position + radius > 800){
+    		Y_position = 800 - radius;	
+    		direction = 360 - direction;
+    		velocity *= elasticity;		
+    		
+    		X_speed = (float)Math.cos(Math.toRadians(direction)) * velocity;
+        	Y_speed = (float)Math.sin(Math.toRadians(direction)) * velocity;
+    	}
+    	
     }
     
     public void setGarvity(float gravity){
@@ -117,15 +136,17 @@ public class Ball{
 		}
 	}
 	public boolean checkBallCollision(float x, float y, float radius){
+//		Log.d("Ball 1", "x = " + this.X_position + " y = " + this.Y_position + " radius = " + this.radius);
+//		Log.d("Ball 2", "x = " + x + " y = " + y + " radius = " + radius);
 		if((Math.pow(Math.pow(this.X_position - x, 2) + Math.pow(this.Y_position - y, 2),0.5) <= this.radius + radius) ){
+			Log.d("Ball", "collision true");
 			return true;
 		}
+		//Log.d("Ball", "collision false");
 		return false;
 	}
 	
-	public void resolveCollision(Ball ball1, Ball ball2){
-		
-	}
+	
 	
 	public float getX(){
 		return this.X_position;
@@ -135,5 +156,96 @@ public class Ball{
 	}
 	public float getRadius(){
 		return this.radius;
+	}
+	public float getDirection(){
+		return this.direction;
+	}
+	public float getVelocity() {
+		return velocity;
+	}
+	public void setVelocity(float velocity) {
+		this.velocity = velocity;
+	}
+	public float getX_speed() {
+		return X_speed;
+	}
+	public void setX_speed(float x_speed) {
+		X_speed = x_speed;
+	}
+	public float getY_speed() {
+		return Y_speed;
+	}
+	public void setY_speed(float y_speed) {
+		Y_speed = y_speed;
+	}
+	public float getX_position() {
+		return X_position;
+	}
+	public void setX_position(float x_position) {
+		X_position = x_position;
+	}
+	public float getY_position() {
+		return Y_position;
+	}
+	public void setY_position(float y_position) {
+		Y_position = y_position;
+	}
+	public float getElasticity() {
+		return elasticity;
+	}
+	public void setElasticity(float elasticity) {
+		this.elasticity = elasticity;
+	}
+	public float getMass() {
+		return mass;
+	}
+	public void setMass(float mass) {
+		this.mass = mass;
+	}
+	public float getGravity() {
+		return gravity;
+	}
+	public void setGravity(float gravity) {
+		this.gravity = gravity;
+	}
+	public int getLife() {
+		return life;
+	}
+	public void setLife(int life) {
+		this.life = life;
+	}
+	public List<Ball> getBalls() {
+		return balls;
+	}
+	public void setBalls(List<Ball> balls) {
+		this.balls = balls;
+	}
+	public Paint getPaint() {
+		return paint;
+	}
+	public void setPaint(Paint paint) {
+		this.paint = paint;
+	}
+	public void setDirection(float direction) {
+		this.direction = direction;
+	}
+	public void setRadius(float radius) {
+		this.radius = radius;
+	}
+	public void copyBall(Ball ball){
+		this.direction = ball.direction;
+		this.elasticity = ball.direction;
+		this.gravity = ball.gravity;
+		this.life = ball.life;
+		this.mass = ball.mass;
+		this.radius = ball.radius;
+		this.velocity = ball.velocity;
+		this.X_position = ball.X_position;
+		this.Y_position = ball.Y_position;
+		this.X_speed = ball.X_speed;
+		this.Y_speed = ball.Y_speed;
+	}
+	public void setColor(int color_value){
+		this.paint.setColor(color_value);
 	}
 }
