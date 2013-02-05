3c9bddfe0918407e3d1ac8fd4026a14241610b44
Szpada <michal.szmorlinski@gmail.com>
2013-02-05 14:36:06 +0100

    Opcje update

--- src/com/gra/menu/OptionsTab2.java
+++ src/com/gra/menu/OptionsTab2.java
@@ -59,7 +59,7 @@ public class OptionsTab2 extends Activity{
         
         //Galeria textur gracza
         player = (Gallery) findViewById(R.id.gallery2);
-        player.setAdapter(new ImageAdapter(this, 100, 100, R.drawable.jez1, R.drawable.jez2, R.drawable.jez3, R.drawable.jez4, R.drawable.moneta, R.drawable.moneta));
+        player.setAdapter(new ImageAdapter(this, 100, 100, R.drawable.jez1, R.drawable.jez2, R.drawable.jez3, R.drawable.jez4));
         player.setOnItemSelectedListener(new OnItemSelectedListener() {
 	
 			public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
@@ -76,7 +76,7 @@ public class OptionsTab2 extends Activity{
         
       //Galeria textur ziemi
         earth = (Gallery) findViewById(R.id.gallery1);
-        earth.setAdapter(new ImageAdapter(this, 100, 100, R.drawable.ziemia1, R.drawable.ziemia2, R.drawable.ziemia3, R.drawable.moneta));
+        earth.setAdapter(new ImageAdapter(this, 100, 100, R.drawable.ziemia1, R.drawable.ziemia2, R.drawable.ziemia3));
         earth.setOnItemSelectedListener(new OnItemSelectedListener() {
 	
 			public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
@@ -88,8 +88,6 @@ public class OptionsTab2 extends Activity{
 			public void onNothingSelected(AdapterView<?> parent) {
 				
 			}
-	
-	
 		});
     }
 }
