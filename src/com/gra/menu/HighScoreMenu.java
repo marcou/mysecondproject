fa1ec60840ab77553229b16bad76b633bd7d8007
Szpada <michal.szmorlinski@gmail.com>
2013-03-07 21:10:01 +0100

    HighScore
    Klasa do przechowywania rekordu (Record), zaimplementowane sortowanie i
    dodawanie posortowanej listy. Problem z layoutem - nie mogę ogarnąć żeby
    ta kurwa przyklejała się do prawej.

--- src/com/gra/menu/HighScoreMenu.java
+++ src/com/gra/menu/HighScoreMenu.java
@@ -1,5 +1,9 @@
 package com.gra.menu;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import com.gra.R;
 
 import android.app.Activity;
@@ -8,6 +12,7 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TableRow.LayoutParams;
@@ -16,6 +21,7 @@ import android.widget.TextView;
 public class HighScoreMenu extends Activity{
 	
 	private TableLayout score;
+	private List<Record> records;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
@@ -29,64 +35,55 @@ public class HighScoreMenu extends Activity{
         
         score = (TableLayout)findViewById(R.id.highScoreTable);
         
-        addRecord(1, "pies", 100000);
-        addRecord(2, "kon", 9999);
-        addRecord(3, "okon", 2345);
-        addRecord(4, "wieloryb", 2338);
-        addRecord(5, "plaszczka", 107);
-        addRecord(6, "rys", 85);
-        addRecord(7, "bawol", 76);
-        addRecord(8, "mysz", 13);
-        addRecord(9, "golab", 9);
-        addRecord(10, "mis", 6);
-        addRecord(0, "to", 0);
-        addRecord(0, "jest", 0);
-        addRecord(0, "tylko", 0);
-        addRecord(0, "test", 0);
-        addRecord(0, "zeby", 0);
-        addRecord(0, "sparwdzic", 0);
-        addRecord(0, "czy", 0);
-        addRecord(0, "scrollowanie", 0);
-        addRecord(0, "dziala", 0);
-        addRecord(0, ".", 0);
-        addRecord(0, "Jak", 0);
-        addRecord(0, "widac", 0);
-        addRecord(0, "dziala", 0);
-        addRecord(0, "i", 0);
-        addRecord(0, "to", 0);
-        addRecord(0, "calkiem", 0);
-        addRecord(0, "niezle", 0);
-        addRecord(0, "bo wczytal", 0);
-        addRecord(0, "dokladnie", 0);
-        addRecord(0, "31", 0);
-        addRecord(0, "rekordow", 0);
+        records = new ArrayList<Record>();
         
+        records.add(new Record("pies", 431));
+        records.add(new Record("kon", 5351));
+        records.add(new Record("okon", 17436));
+        records.add(new Record("wieloryb", 164));
+        records.add(new Record("plaszczka", 64164));
+        records.add(new Record("rys", 7781));
+        for(int i = 0; i < 94; i++){
+        	records.add(new Record("test" + i, i + 1));
+        }
+        
+        sortRecords();
+        
+        for(int i = 0; i < records.size(); i ++){
+        	addRecord(i+1, records.get(i).getName(), records.get(i).getPoints());
+        }
+	}
+
+	@SuppressWarnings("unchecked")
+	private void sortRecords() {
+		Collections.sort(records);
 	}
 	
 	public void addRecord(int rank, String name, long points){
 		
 		TableRow tr = new TableRow(this);
-        LayoutParams rankParamas = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT | Gravity.LEFT);
+        LayoutParams rankParamas = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT | Gravity.RIGHT);
         LayoutParams otherParams = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
         tr.setLayoutParams(otherParams);
 
         TextView tvLeft = new TextView(this);
-        rankParamas.setMargins(80, 0, 0, 0);
+        rankParamas.weight = 1.0f;
+        //rankParamas.setMargins(20, 0, 40, 0);
         tvLeft.setLayoutParams(rankParamas);
         tvLeft.setTextSize(15.0f);
         tvLeft.setText(Integer.toString(rank));
         //tvLeft.setText("1");
         
         TextView tvCenter = new TextView(this);
-        otherParams.setMargins(180, 0, 0, 0);
-        tvCenter.setLayoutParams(otherParams);
+        //rankParamas.setMargins(140, 0, 0, 0);
+        tvCenter.setLayoutParams(rankParamas);
         tvCenter.setTextSize(15.0f);
         tvCenter.setText(name);
         //tvCenter.setText("2");
         
         TextView tvRight = new TextView(this);
-        otherParams.setMargins(220, 0, 0, 0);
-        tvRight.setLayoutParams(otherParams);
+        //rankParamas.setMargins(200, 0, 0, 0);
+        tvRight.setLayoutParams(rankParamas);
         tvRight.setTextSize(15.0f);
         tvRight.setText(Long.toString(points));
         //tvRight.setText("3");
@@ -95,6 +92,47 @@ public class HighScoreMenu extends Activity{
         tr.addView(tvCenter);
         tr.addView(tvRight);
 
-        score.addView(tr, new TableLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+        //score.addView(tr);
+        score.addView(tr, new TableLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT | Gravity.LEFT));
+	}
+	
+	/**
+	 * 
+	 * @author Szpada
+	 * Klasa zawierajaca nazwe gracza wraz z punkatmi ktore uzyskal
+	 */
+	@SuppressWarnings("rawtypes")
+	public class Record implements Comparable{
+		private String name;
+		private long points;
+		
+		public Record(String name, long points){
+			this.name = name;
+			this.points = points;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public long getPoints() {
+			return points;
+		}
+
+		public void setPoints(long points) {
+			this.points = points;
+		}
+
+		public int compareTo(Object another) {
+			Record record = (Record) another;
+			
+			if(this.points > record.getPoints()) return -1;	//normalnie powinno byc 1 ale mamy odwrotna kolejnosc
+			else if(this.points < record.getPoints()) return 1; //a tu -1
+			else return 0;
+		}
 	}
 }
